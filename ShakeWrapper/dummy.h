1142225739e2dfd0eed578c8bd3f717b0f6ef86f
David <david.halapir@decode.agency>
2021-12-02 09:57:15 +0100

    SHK-000-basic target wrapper

---
+++ ShakeWrapper/dummy.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  
+//
+//  Created by David Halapir on 02.12.2021..
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ ShakeWrapper/dummy.m
@@ -0,0 +1,10 @@
+//
+//  File.m
+//  
+//
+//  Created by David Halapir on 02.12.2021..
+//
+
+#import <Foundation/Foundation.h>
+#import "dummy.h"
+
